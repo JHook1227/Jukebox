a062a9229dd109412c62d6ac2a4a40cc6d8ecd95
Joe Hook <jhook1173@gmail.com>
2024-04-25 19:08:45 +0000

    Commit2

--- Jukebox.h
+++ Jukebox.h
@@ -20,7 +20,7 @@ struct Playlist {
     vector<Song> songs;
 };
 struct Jukebox {
-    vector <Genres> genres;
+    vector<Genre> genres;
     int curr_genre_idx;
     int curr_playlist_idx;
     int curr_song_idx;
@@ -31,16 +31,14 @@ struct Jukebox {
     
     void play_pause(){
         isplaying = !isPlaying;
+    }
     
     void addgenre(Genre& add_genre){
         genre.push_back(add_genre);
         cout << add_genre << " added  to playlist.";
     }
         
-    void skip_to_prev(){
-        if (curr_song_idx > 0){
-            curr_song_idx--;
-            //
+    
     void addsongtogenre(string genrename, Song* new_song){
         for(Genre* genre : genres){
             if(genre.name == genrename){
@@ -53,7 +51,69 @@ struct Jukebox {
             }
         }
     }
-    void play(
+    void skip_to_prev_track(){
+        if (curr_song_idx > 0){
+            curr_song_idx--;
+        }
+        else if (curr_playlist_idx > 0){
+            curr_playlist_idx--;
+            curr_song_idx = 0;
+        }
+        else if (curr_genre_idx > 0){
+            curr_genre_idx--;
+            curr_playlist_idx = 0;
+            curr_song_idx = 0;
+        }
+    }
+    void skip_to_next_track(){
+        Genre* curr_genre = genres[curr_genre_idx];
+        Song* curr_song = songs[curr_song_idx];
+        if (curr_song_idx < songs.size() - 1){
+            curr_song_idx++;
+        }
+        else if (curr_playlist_idx < playlists.size() - 1){
+            curr_playlist_idx++;
+            curr_song_idx = 0;
+        }
+        else if (curr_genre_idx < genres.size() - 1){
+            curr_genre_idx++;
+            curr_playlist_idx = 0;
+            curr_song_idx = 0;
+        }
+        else if (isonrepeat){
+            curr_song_idx = 0;
+        }
+    }
+    void skip_to_first_track_curr_playlist(){
+        curr_song_idx = 0;
+    }
+    void skip_to_first_track_prev_playlist(){
+        if (curr_playlist_idx > 0){
+            curr_playlist_idx--;
+            curr_song_idx = 0;
+        }
+        else if (curr_genre_idx > 0){
+            curr_genre_idx--;
+            curr_playlist_idx = 0;
+            curr_song_idx = 0;
+        }
+        else{
+            curr_genre_idx = genres.size() - 1;
+            curr_playlist_idx = 0;
+            curr_song_idx = 0;
+        }
+    }
+    void skip_to_first_track_next_playlist(){
+        if (curr_playlist_idx < playlist.size() - 1){
+            curr_playlist_idx++;
+            curr_song_idx = 0;
+        }
+    }
+    
+            
+
+                
+                
         
 };
 
