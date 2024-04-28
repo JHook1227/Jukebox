6b43613872660162a28d4089cd9dbcfc924465af
Joe Hook <jhook1173@gmail.com>
2024-04-28 16:36:11 +0000

    Commit 9

--- main.cpp
+++ main.cpp
@@ -34,6 +34,15 @@ struct Jukebox {
     
     void play_pause(){
         isplaying = !isplaying;
+        Genre& curr_genre = genres[curr_genre_idx];
+        Playlist& curr_playlist = curr_genre.playlists[curr_playlist_idx];
+        Song& curr_song = curr_playlist.songs[curr_song_idx];
+        if (isplaying == true){
+            cout << curr_song.title << " by " << curr_song.artist << " is playing." << endl;
+        }
+        else{
+            cout << "Paused." << endl;
+    }
     }
     
     void repeat(){
@@ -128,7 +137,10 @@ struct Jukebox {
         }
     }
 }
-    //void Jukebox::press_play() {
+    size_t get_current_playlist_index() {
+        return curr_playlist_idx;
+    }
+    //void press_play() {
     //const Song& curr_song = get_curr_song();
     //cout << curr_song.title << " by " << curr_song.artist << " is playing. " << endl;
 
@@ -153,29 +165,129 @@ struct Jukebox {
 int main() {
     
     Jukebox jukebox;
-    Genre genre1;
-    genre1.name = "Rock";
-
-    Genre genre2;
-    genre2.name = "Pop";
-
-    jukebox.genres.push_back(genre1);
-    jukebox.genres.push_back(genre2);
-
-    Playlist playlist1;
-    playlist1.name = "Classic Rock";
-    playlist1.songs.push_back({"Stairway to Heaven", "Led Zeppelin"});
-    playlist1.songs.push_back({"Bohemian Rhapsody", "Queen"});
-
-    Playlist playlist2;
-    playlist2.name = "Top Hits";
-    playlist2.songs.push_back({"Shape of You", "Ed Sheeran"});
-    playlist2.songs.push_back({"Blinding Lights", "The Weeknd"});
-
-    jukebox.genres[0].playlists.push_back(playlist1);
-    jukebox.genres[1].playlists.push_back(playlist2);
+    int choice;
+    int exit = 1;
     
-    jukebox.print_playlists();
+    do {
+        cout << "1. Play/Pause" << endl;
+        cout << "2. Skip to Previous Track" << endl;
+        cout << "3. Skip to Next Track" << endl;
+        cout << "4. Skip to First Track of Current Playlist" << endl;
+        cout << "5. Skip to First Track of Previous Playlist" << endl;
+        cout << "6. Skip to First Track of Next Playlist" << endl;
+        cout << "7. Rearrange Playlists" << endl;
+        cout << "8. Toggle Repeat" << endl;
+        cout << "9. Create Genre" << endl;
+        cout << "10. Create Playlist" << endl;
+        cout << "11. Add song to Playlist" << endl;
+        cout << "12. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if (exit == 0){
+            break;
+        }
+        
+        switch (choice) {
+            case 1:
+                jukebox.play_pause();
+                break;
+            case 2:
+                jukebox.skip_to_prev_track();
+                break;
+            case 3:
+                jukebox.skip_to_next_track();
+                break;
+            case 4:
+                jukebox.skip_to_first_track_curr_playlist();
+                break;
+            case 5:
+                jukebox.skip_to_first_track_prev_playlist();
+                break;
+            case 6:
+                jukebox.skip_to_first_track_next_playlist();
+                break;
+            case 7:
+                size_t new_idx;
+                cout << "Choose a new spot for the current playlist from 1 to " << jukebox.genres.size() << endl;
+                cin >> new_idx;
+                jukebox.rearrange_playlists(jukebox.get_current_playlist_index(), new_idx);
+                cout << "Playlist rearranged" << endl;
+                break;
+            case 8:
+                jukebox.repeat();
+                break;
+            case 9:
+                {
+                string genre_name;
+                cout << "Enter genre name: ";
+                cin.ignore();
+                getline(cin, genre_name);
+                Genre genre;
+                genre.name = genre_name;
+                jukebox.genres.push_back(genre);
+                cout << "Genre created: " << genre_name << endl;
+                    }
+                break;
+            case 10:
+                        {
+                            string genre_name;
+                            cout << "Enter genre name: ";
+                            cin.ignore();
+                            getline(cin, genre_name);
+                            for (Genre& genre : jukebox.genres){
+                                if (genre.name == genre_name){
+                                    string playlist_name;
+                                    cout << "Enter playlist name: ";
+                                    getline(cin, playlist_name);
+                                    Playlist playlist;
+                                    playlist.name = playlist_name;
+                                    genre.playlists.push_back(playlist);
+                                    cout << "Playlist created: " << playlist_name << endl;
+                                    break;
+                                }
+                            }
+                        }
+                break;
+            case 11: 
+                {
+                    string genre_name, playlist_name;
+                    cout << "Enter genre name: ";
+                    cin.ignore();
+                    getline(cin, genre_name);
+                    cout << "Enter playlist name: ";
+                    getline(cin, playlist_name);
+                    for (Genre& genre : jukebox.genres){
+                        if (genre.name == genre_name){
+                            for (Playlist& playlist : genre.playlists){
+                                if (playlist.name == playlist_name){
+                                    string song_title, song_artist;
+                                    cout << "Enter song title: ";
+                                    getline(cin, song_title);
+                                    cout << "Enter song artist: ";
+                                    getline(cin, song_artist);
+                                    Song song;
+                                    song.title = song_title;
+                                    song.artist = song_artist;
+                                    playlist.songs.push_back(song);
+                                    cout << song_title << " by " << song_artist << " added to " << playlist_name << " in " << genre_name << endl;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                    break;
+            case 12:
+                cout << "Exiting" << endl;
+                jukebox.print_playlists();
+                exit = 0;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+        }
+    }while (true);
     
     return 0;
 }
